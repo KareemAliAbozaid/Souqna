12c0eaa028b1b467f9af21bf408f7af724acabaf
Kareem Abozaid <karimabozaid487@gmail.com>
2026-02-24 16:23:20 +0200

    Edit

--- Souqna.Application/ApplicationServiceRegistration.cs
+++ Souqna.Application/ApplicationServiceRegistration.cs
@@ -6,7 +6,6 @@ namespace Souqna.Application
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            // Register AutoMapper - scans for all mapping profiles in this assembly
             services.AddAutoMapper(typeof(ApplicationServiceRegistration).Assembly);
 
             return services;
